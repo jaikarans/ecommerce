0b92883cf977b5474a761390bf40ca3d49d8d170
jaikaran saini <jaikrns@gmail.com>
2022-04-09 19:47:52 +0530

    trying normlize-port module

--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
 const http = require('http');
 const dotenv = require('dotenv');
+const normalizePort = require('normalize-port');
 const app = require('./app');
 
 dotenv.config();
 
-const { PORT } = process.env;
+const PORT = normalizePort(process.env.PORT || 5000);
 
 const server = http.createServer(app);
 
